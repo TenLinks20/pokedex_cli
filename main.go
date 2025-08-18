68613db63e60a69e2b5092db1a457ddc2f829eed
TenLinks20 <uzonduberto94@gmail.com>
2025-08-18 10:47:32 +0200

    repl feedback loop

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		words := cleanInput(userInput)
+
+		if len(words) == 0 {
+			fmt.Println("No input provided")
+		} else {
+			fmt.Printf("Your command was: %s\n", words[0])
+		}
+
+	}
 }
